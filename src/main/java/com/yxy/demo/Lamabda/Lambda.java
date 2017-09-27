4cfc928bc544e854a5579cf80947a6a1de8ac043
xiangyu.yzy <xiangyu.yzy@alibaba-inc.com>
2017-09-27 11:43:28 +0800

    Lambda

---
+++ src/main/java/com/yxy/demo/Lamabda/Lambda.java
@@ -0,0 +1,16 @@
+package com.yxy.demo.Lamabda;
+
+import java.util.Arrays;
+
+/**
+ * @author yanxiangyuxx
+ * @create 2017-09-27
+ **/
+public class Lambda {
+
+    public static void main(String[] args){
+
+
+        Arrays.asList( "a", "b", "d" ).forEach( e -> System.out.println( e ) );
+    }
+}
